f29778e046f37e42513b44c7f77ddd3e87f5eda4
tomcohen <66440442+tomcoh5@users.noreply.github.com>
2021-08-06 15:14:40 +0300

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,1 +1,20 @@
-AWS_REGION="eu-west-1"
+provider "aws" {
+
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+   eu-west-1 = "my ami"
+ }
+}
+
+resource "aws_instance" "example" {
+  ami = "${var.AMIS[var.AWS_REGION]}"
+  instance_type = "t2.micro"
+
+}
