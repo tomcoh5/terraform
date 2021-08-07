689b052e39244062dfbd64bfbb28f0b00fbddebb
tomcohen <66440442+tomcoh5@users.noreply.github.com>
2021-08-07 13:23:03 +0300

    Update instance.tf

--- software-provision/instance.tf
+++ software-provision/instance.tf
@@ -1,25 +1,25 @@
-resource "aws_key_pair" "mykey" {
+resource "aws_key_pair" "mykey" { # path to key
   key_name   = "mykey"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example" { # creating aws instance
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
   key_name      = aws_key_pair.mykey.key_name
 
-  provisioner "file" {
+  provisioner "file" { #copying file 
     source      = "script.sh"
     destination = "/tmp/script.sh"
   }
-  provisioner "remote-exec" {
+  provisioner "remote-exec" { #executing script 
     inline = [
       "chmod +x /tmp/script.sh",
       "sudo sed -i -e 's/\r$//' /tmp/script.sh",  # Remove the spurious CR characters.
       "sudo /tmp/script.sh",
     ]
   }
-  connection {
+  connection { $ #connection with ssh 
     host        = coalesce(self.public_ip, self.private_ip)
     type        = "ssh"
     user        = var.INSTANCE_USERNAME
